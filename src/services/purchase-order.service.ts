e9b00d72b71aeead4981140c88085402e862004e
askribilel123 <askribilel09@gmail.com>
2026-05-29 10:23:26 +0100

    create purchase order service

---
+++ src/services/purchase-order.service.ts
@@ -0,0 +1,52 @@
+import axiosInstance from "../api/axiosInstance";
+
+export type PurchaseOrder = {
+  id: number;
+  quantity_received: number;
+  quantity_ordered: number;
+  supplier_id: number;
+  created_by: number;
+  status: string;
+  createdAt: string;
+  updatedAt: string;
+  deletedAt: string | null;
+};
+
+export type CreatePurchaseOrderDto = {
+  quantity_ordered: number;
+  supplier_id: number;
+};
+
+export type PurchaseOrderData = {
+  success: boolean;
+  purchaseOrders: {
+    data: PurchaseOrder[];
+    page: number;
+    total: number;
+    totalPages: number;
+  };
+};
+
+export const purchaseOrderService = {
+  getAll: () =>
+    axiosInstance
+      .get<PurchaseOrderData>("/purchase-orders")
+      .then((r) => r.data),
+  getById: (id: number) =>
+    axiosInstance
+      .get<{
+        success: boolean;
+        purchaseOrder: PurchaseOrder;
+      }>(`/purchase-orders/${id}`)
+      .then((r) => r.data),
+  update: (id: number, data: CreatePurchaseOrderDto) =>
+    axiosInstance
+      .put<{
+        success: boolean;
+        purchaseOrder: PurchaseOrder;
+      }>(`/purchase-orders/${id}`, data)
+      .then((r) => r.data),
+  remove: (id: number) => axiosInstance.delete(`/purchase-orders/${id}`),
+  create: (data: CreatePurchaseOrderDto) =>
+    axiosInstance.post("/purchase-orders", data),
+};
