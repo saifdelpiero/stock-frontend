52fe748ade94ee7bc2ee3173c028fcb17d669de2
askribilel123 <askribilel09@gmail.com>
2026-05-31 21:52:21 +0100

    update warehouse

--- src/pages/Warehouses/EditWarehouse.tsx
+++ src/pages/Warehouses/EditWarehouse.tsx
@@ -8,27 +8,28 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import {
-  CreateSupplierDto,
-  Supplier,
-  supplierService,
-} from "../../services/supplier.service";
+  CreateWarehouseDto,
+  Warehouse,
+  warehouseService,
+} from "../../services/warehouse.service";
 
-const createSupplierSchema = z.object({
+const createWarehouseSchema = z.object({
   name: z
     .string()
     .trim()
     .min(2, "Name is required and must be at least 2 characters")
     .max(100),
-  phone: z
+  type: z
     .string()
     .trim()
-    .min(2, "Phone is required and must be at least 2 characters")
+    .min(2, "Type is required and must be at least 2 characters")
     .max(100),
   address: z
     .string()
     .trim()
     .min(2, "Address is required and must be at least 2 characters")
     .max(255),
+  min_stock: z.number().min(0, "Minimum stock must be a non-negative number"),
 });
 
 export default function EditWarehouse() {
@@ -43,13 +44,15 @@ export default function EditWarehouse() {
     register,
     handleSubmit,
     reset,
+    getValues,
     formState: { errors },
-  } = useForm<CreateSupplierDto>({
-    resolver: zodResolver(createSupplierSchema),
+  } = useForm<CreateWarehouseDto>({
+    resolver: zodResolver(createWarehouseSchema),
     defaultValues: {
       name: "",
-      phone: "",
+      type: "",
       address: "",
+      min_stock: 0,
     },
   });
 
@@ -57,13 +60,14 @@ export default function EditWarehouse() {
     if (!id) return;
     const controller = new AbortController();
     setStatus("loading");
-    supplierService
+    warehouseService
       .getById(parseInt(id))
-      .then((supplierData: { success: boolean; supplier: Supplier }) => {
+      .then((warehouseData: { success: boolean; warehouse: Warehouse }) => {
         reset({
-          name: supplierData.supplier.name,
-          phone: supplierData.supplier.phone,
-          address: supplierData.supplier.address,
+          name: warehouseData.warehouse.name,
+          type: warehouseData.warehouse.type,
+          address: warehouseData.warehouse.address,
+          min_stock: warehouseData.warehouse.min_stock,
         });
         setStatus("success");
       })
@@ -79,21 +83,21 @@ export default function EditWarehouse() {
     return () => controller.abort();
   }, [id]);
 
-  const handleUpdateSupplier = async (formData: CreateSupplierDto) => {
+  const handleUpdateWarehouse = async (formData: CreateWarehouseDto) => {
     if (!id) {
-      setError("Invalid supplier ID.");
+      setError("Invalid warehouse ID.");
       return;
     }
     setStatus("loading");
     try {
-      await supplierService.update(parseInt(id), formData);
+      await warehouseService.update(parseInt(id), formData);
       setStatus("success");
-      navigate("/suppliers");
+      navigate("/warehouses");
     } catch (error: any) {
       setStatus("error");
       setError(
         error?.["response"]?.["data"]?.["error"] ||
-          "An error occurred while updating the supplier.",
+          "An error occurred while updating the warehouse.",
       );
       setTimeout(() => {
         setError(null);
@@ -115,13 +119,13 @@ export default function EditWarehouse() {
   return (
     <div>
       <PageMeta
-        title="Stock Management System | Update Supplier"
-        description="Stock Management System Update Supplier Page"
+        title="Stock Management System | Update Warehouse"
+        description="Stock Management System Update Warehouse Page"
       />
-      <PageBreadcrumb pageTitle="Update Supplier" />
+      <PageBreadcrumb pageTitle="Update Warehouse" />
       <div className="grid grid-cols-1 gap-6 ">
         <div className="space-y-6">
-          <ComponentCard title="Update Supplier">
+          <ComponentCard title="Update Warehouse">
             {error && (
               <div className="">
                 <div
@@ -150,7 +154,7 @@ export default function EditWarehouse() {
             <form
               autoComplete="off"
               className="space-y-6"
-              onSubmit={handleSubmit(handleUpdateSupplier)}
+              onSubmit={handleSubmit(handleUpdateWarehouse)}
             >
               <div>
                 <Label htmlFor="name">Name</Label>
@@ -160,28 +164,11 @@ export default function EditWarehouse() {
                     id="name"
                     className={inputClasses}
                     {...register("name")}
-                    placeholder="Enter Supplier Name"
+                    placeholder="Enter Warehouse Name"
                   />
                   {errors.name && (
                     <p className="text-red-500 text-sm mt-1">
                       {errors.name.message}
-                    </p>
-                  )}
-                </div>
-              </div>
-              <div>
-                <Label htmlFor="phone">Phone Number</Label>
-                <div className="relative">
-                  <input
-                    type="text"
-                    id="phone"
-                    className={inputClasses}
-                    {...register("phone")}
-                    placeholder="Enter Phone Number"
-                  />
-                  {errors.phone && (
-                    <p className="text-red-500 text-sm mt-1">
-                      {errors.phone.message}
                     </p>
                   )}
                 </div>
@@ -206,11 +193,70 @@ export default function EditWarehouse() {
               </div>
 
               <div>
+                <Label>Select Type</Label>
+                <select
+                  {...register("type")}
+                  className={`h-11 w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 pr-11 text-sm shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800 ${
+                    getValues("type")
+                      ? "text-gray-800 dark:text-white/90"
+                      : "text-gray-400 dark:text-gray-400"
+                  } dark:bg-dark-900`}
+                >
+                  {/* Placeholder option */}
+                  <option
+                    value=""
+                    disabled
+                    hidden
+                    className="text-gray-700 dark:bg-gray-900 dark:text-gray-400"
+                  >
+                    Select an option
+                  </option>
+                  {/* Map over options */}
+
+                  <option
+                    value="physique"
+                    className="text-gray-700 dark:bg-gray-900 dark:text-gray-400"
+                  >
+                    Physique
+                  </option>
+                  <option
+                    value="logic"
+                    className="text-gray-700 dark:bg-gray-900 dark:text-gray-400"
+                  >
+                    logic
+                  </option>
+                </select>
+                {errors.type && (
+                  <p className="text-red-500 text-sm mt-1">
+                    {errors.type.message}
+                  </p>
+                )}
+              </div>
+
+              <div>
+                <Label>Minimum Stock</Label>
+                <div className="relative">
+                  <input
+                    type="text"
+                    id="min_stock"
+                    className={inputClasses}
+                    {...register("min_stock", { valueAsNumber: true })}
+                    placeholder="Enter Minimum Stock"
+                  />
+                  {errors.min_stock && (
+                    <p className="text-red-500 text-sm mt-1">
+                      {errors.min_stock.message}
+                    </p>
+                  )}
+                </div>
+              </div>
+
+              <div>
                 <button
                   type="submit"
                   className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700  dark:text-gray-400"
                 >
-                  Update Supplier
+                  Update Warehouse
                 </button>
               </div>
             </form>
